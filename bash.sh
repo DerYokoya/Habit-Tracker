dc0bc6a0d8f0896c33951ee0dbcaee42684ce1a1
DerYokoya <133297414+DerYokoya@users.noreply.github.com>
2026-05-16 20:57:37 -0400

    Initial commit

---
+++ bash.sh
@@ -0,0 +1,4 @@
+# Create a new React app for the Chrome extension
+npx create-react-app habit-tracker-extension
+cd habit-tracker-extension
+npm install @hello-pangea/dnd recharts date-fns lucide-react
